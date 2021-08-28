bd82fc9716ea6bb8c9cbcde7584feb96083ec4c7
Burt Beckwith <burt@burtbeckwith.com>
2021-08-28 10:37:22 -0400

    expose port

--- mongo-reactive/src/test/groovy/io/micronaut/configuration/mongo/reactive/MongoHealthIndicatorSpec.groovy
+++ mongo-reactive/src/test/groovy/io/micronaut/configuration/mongo/reactive/MongoHealthIndicatorSpec.groovy
@@ -16,33 +16,38 @@
 package io.micronaut.configuration.mongo.reactive
 
 import com.mongodb.reactivestreams.client.MongoClient
-import io.micronaut.configuration.mongo.core.MongoSettings
 import io.micronaut.configuration.mongo.reactive.health.MongoHealthIndicator
 import io.micronaut.context.ApplicationContext
 import io.micronaut.core.io.socket.SocketUtils
-import io.micronaut.health.HealthStatus
 import io.micronaut.management.health.indicator.HealthResult
 import org.testcontainers.containers.GenericContainer
 import reactor.core.publisher.Flux
 import spock.lang.Specification
+
 import java.time.Duration
-import java.time.temporal.ChronoUnit
+
+import static io.micronaut.configuration.mongo.core.MongoSettings.MONGODB_URI
+import static io.micronaut.health.HealthStatus.DOWN
+import static io.micronaut.health.HealthStatus.UP
+import static java.time.temporal.ChronoUnit.SECONDS
 
 class MongoHealthIndicatorSpec extends Specification {
 
     void "test mongo health indicator DOWN"() {
         when:
         ApplicationContext applicationContext = ApplicationContext.run(
-                (MongoSettings.MONGODB_URI): "mongodb://localhost:${SocketUtils.findAvailableTcpPort()}"
+                (MONGODB_URI): "mongodb://localhost:${SocketUtils.findAvailableTcpPort()}"
         )
         try {
-            Flux.from(applicationContext.getBean(MongoClient).listDatabaseNames()).timeout(Duration.of(2, ChronoUnit.SECONDS)).blockFirst()
-        } catch (e) {
+            Flux.from(applicationContext.getBean(MongoClient).listDatabaseNames())
+                    .timeout(Duration.of(2, SECONDS))
+                    .blockFirst()
+        } catch (ignored) {
         }
         MongoHealthIndicator healthIndicator = applicationContext.getBean(MongoHealthIndicator)
 
         then:
-        Flux.from(healthIndicator.result).blockFirst().status == HealthStatus.DOWN
+        Flux.from(healthIndicator.result).blockFirst().status == DOWN
 
         cleanup:
         applicationContext.close()
@@ -50,24 +55,26 @@ class MongoHealthIndicatorSpec extends Specification {
 
     void "test mongo health indicator UP"() {
         given:
-        GenericContainer mongo =
-                new GenericContainer("mongo:4.0")
+        GenericContainer mongo = new GenericContainer("mongo:4.0")
+                .withExposedPorts(27017)
         mongo.start()
 
         when:
         ApplicationContext applicationContext = ApplicationContext.run(
-                (MongoSettings.MONGODB_URI): "mongodb://${mongo.containerIpAddress}:${mongo.getMappedPort(27017)}"
+                (MONGODB_URI): "mongodb://$mongo.containerIpAddress:${mongo.getMappedPort(27017)}"
         )
         try {
-            Flux.from(applicationContext.getBean(MongoClient).listDatabaseNames()).timeout(Duration.of(2, ChronoUnit.SECONDS)).blockFirst()
-        } catch (e) {
+            Flux.from(applicationContext.getBean(MongoClient).listDatabaseNames())
+                    .timeout(Duration.of(2, SECONDS))
+                    .blockFirst()
+        } catch (ignored) {
         }
         MongoHealthIndicator healthIndicator = applicationContext.getBean(MongoHealthIndicator)
 
         HealthResult healthResult = Flux.from(healthIndicator.result).blockFirst()
 
         then:
-        healthResult.status == HealthStatus.UP
+        healthResult.status == UP
         healthResult.details.containsKey("mongodb (Primary)")
 
         cleanup:
